af769aa94ed79160f94580e5672d043ca14aa087
Lucas Eufrasio <lucaswilliameufrasio@gmail.com>
2024-08-12 18:49:07 -0300

    chore: add SwitftUI project boilerplate

---
+++ Art Institute of Chicago/Art_Institute_of_ChicagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_Institute_of_ChicagoApp.swift
+//  Art Institute of Chicago
+//
+//  Created by Yoda on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Art_Institute_of_ChicagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
